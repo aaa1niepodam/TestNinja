091309905de2c2feadd485d5a97ca7e1ea47258c
Fabio Tavoloni <fabio.t0844.beyond@xpi.com.br>
2018-07-19 08:30:11 -0300

    MSTest

---
+++ TestNinja.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestNinja.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestNinja.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f02c54b1-af6f-4952-a877-852ca4d69826")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- TestNinja/Fundamentals/Reservation.cs
+++ TestNinja/Fundamentals/Reservation.cs
@@ -4,11 +4,8 @@
     {
         public User MadeBy { get; set; }
 
-        public bool CanBeCancelledBy(User user)
-        {
-            return (user.IsAdmin || MadeBy == user);
-        }
-        
+        //three scenarios (execution paths)
+        public bool CanBeCancelledBy(User user) => (user.IsAdmin || MadeBy == user);
     }
 
     public class User
