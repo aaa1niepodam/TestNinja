e0823fe32687f3811642d1a43c05df28bc4d259f
Fabio Tavoloni <fabio.t0844.beyond@xpi.com.br>
2018-07-26 09:51:47 -0300

    Dependency injection strategies

---
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class VideoServiceTests
+    {
+        [Test]
+        public void ReadVideoTitle_EmptyFile_ReturnError()
+        {
+            var service = new VideoService(new FakeFileReader());
+            //service.FileReader = new FakeFileReader();
+            //var result = service.ReadVideoTitle(new FakeFileReader());
+            
+            var result = service.ReadVideoTitle();
+
+            Assert.That(result, Does.Contain("error").IgnoreCase);
+        }
+    }
+}
--- TestNinja/Mocking/VideoService.cs
+++ TestNinja/Mocking/VideoService.cs
@@ -9,9 +9,20 @@ namespace TestNinja.Mocking
 {
     public class VideoService
     {
+        private IFileReader _fileReader;
+
+        public VideoService(IFileReader fileReader = null)
+        {
+            _fileReader = fileReader ?? new FileReader();
+        }
+
         public string ReadVideoTitle()
         {
-            var str = File.ReadAllText("video.txt");
+            //var str = File.ReadAllText("video.txt"); //external resource
+            //var str = new FileReader().Read("video.txt"); //VideoService tightly-coupled with FileReader
+            //var str = fileReader.Read("video.txt");
+            var str = _fileReader.Read("video.txt");
+
             var video = JsonConvert.DeserializeObject<Video>(str);
             if (video == null)
                 return "Error parsing the video.";
